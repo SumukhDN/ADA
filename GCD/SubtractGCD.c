87b978a0e30aa536385b732d2b3a8b39c2fe95cb
SUMUKH D N <158539917+SumukhDN@users.noreply.github.com>
2024-02-07 17:09:51 +0530

    Create SubtractGCD.c

---
+++ GCD/SubtractGCD.c
@@ -0,0 +1,19 @@
+//Continously subtract smaller integer from the larger one until they become equal and result in GCD
+
+#include <stdio.h>
+
+int main() {
+    int n1, n2;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+    
+    while (n1 != n2) {
+        if (n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+    
+    printf("GCD = %d", n1);
+    return 0;
+}
